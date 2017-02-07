1af691d37d3d49ce7ee9b5ebe07da353431fa538
NehemiasEC <neli92santscz@gmail.com>
2017-02-07 16:55:39 -0500

    theme nehemiaseec

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+	define("TEMPPATH",get_bloginfo("stylesheet_directory"));
+	define("CSS", TEMPPATH . "/css/");
+	define("IMG", TEMPPATH . "/img/");
+	define("JS",TEMPPATH . "/js/");
+
+
+	
+
+ ?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title><?php bloginfo("title")?></title>
+		<link rel="stylesheet" href="<?php print CSS?>normalize.css">
+		<link rel="stylesheet" href="<?php print CSS?>main.css">
+	</head>
+	<body>
+		<header>
+			<figure>
+				<img src="<?php print IMG?>profile.jpeg" alt="">
+			</figure>
+			<h1>UX Designer Frontend and Wordpress Developer!!</h1>
+		</header>
+		<section id="main">
+			<section id="personalinfo">
+				<figure><img src="<?php print IMG?>user.png" alt=""></figure><h1>Personal Info</h1>
+			</section>
+			<section id="knowledge">
+				<h1>Knowledge</h1>
+			</section>
+			<section id="portfolio">
+				<h1>Portfolio</h1>
+			</section>
+		</section>
+		<footer>
+			yo soy el footer
+
+		</footer>
+	</body>
+</html>
