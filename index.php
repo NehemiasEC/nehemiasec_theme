9c20203fd0eb0db9086836b456fcdbb273099e6c
NehemiasEC <neli92santscz@gmail.com>
2017-02-07 18:37:08 -0500

    added phone and platzistuden link

--- index.php
+++ index.php
@@ -20,7 +20,10 @@
 				<div>
 					<figure><img src="<?php print IMG?>user.png" alt=""></figure><h1>Personal Info</h1>
 				</div>
-				<p> <i class="fa fa-map-marker" aria-hidden="true"></i> Jamaica, New York</p>
+				<p> <i class="fa fa-map-marker" aria-hidden="true"></i>  Jamaica, New York</p>
+				<p><i class="fa fa-mobile" aria-hidden="true"></i> <a href="tel:+13472162229">347-216-2229</a></p>
+				<p><i class="fa fa-envelope-o" aria-hidden="true"> neli92santscz@gmail.com</i></p>
+				<p><i class="fa fa-graduation-cap" aria-hidden="true"></i> <a href="https://platzi.com/@nescz/" target="_blank">Platzi Student</a></p>
 			</section>
 			<section id="knowledge">
 				<h1>Knowledge</h1>
