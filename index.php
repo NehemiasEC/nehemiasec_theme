160f448449e6ef992ef151eb918e59c89bb5eff8
NehemiasEC <neli92santscz@gmail.com>
2017-02-07 22:56:56 -0500

    added icon knowloedge and portfolio, and html5 semantic

--- index.php
+++ index.php
@@ -14,27 +14,33 @@
 				<img src="<?php print IMG?>profile.jpeg" alt="">
 			</figure>
 			<h1>UX Designer Frontend and Wordpress Developer!!</h1>
+			<h3>Just KISS (Keep it Simple Stupid)</h3>
 		</header>
 		<section id="main">
 			<section id="personalinfo">
 				<div>
-					<figure><img src="<?php print IMG?>user.png" alt=""></figure><h1>Personal Info</h1>
+					<figure><img src="<?php print IMG?>user.png" alt=""></figure><h1> Personal Info</h1>
 				</div>
-				<p> <i class="fa fa-map-marker" aria-hidden="true"></i>  Jamaica, New York</p>
+				<p> <i class="fa fa-map-marker" aria-hidden="true"></i>  Queens, New York</p>
 				<p><i class="fa fa-mobile" aria-hidden="true"></i> <a href="tel:+13472162229">347-216-2229</a></p>
-				<p><i class="fa fa-envelope-o" aria-hidden="true"> neli92santscz@gmail.com</i></p>
+				<p><i class="fa fa-envelope-o" aria-hidden="true"> <a href="mailto:neli92santscz@gmail.com">neli92santscz@gmail.com</a></i></p>
 				<p><i class="fa fa-graduation-cap" aria-hidden="true"></i> <a href="https://platzi.com/@nescz/" target="_blank">Platzi Student</a></p>
 			</section>
 			<section id="knowledge">
-				<h1>Knowledge</h1>
+				<div>
+					<figure><img src="<?php print IMG?>knowledge.png" alt=""></figure><h1> Knowledge</h1>
+				</div>
 			</section>
 			<section id="portfolio">
-				<h1>Portfolio</h1>
+				<div>
+					<figure><img src="<?php print IMG?>portfolio.png" alt=""></figure><h1> Portfolio</h1>
+				</div>
 			</section>
 		</section>
 		<footer>
-			yo soy el footer
-
+			<figure>	
+				<a href="http://www.w3.org/html/logo/"><img src="https://www.w3.org/html/logo/badge/html5-badge-h-css3-device-multimedia-performance-semantics.png" width="261" height="64" alt="HTML5 Powered with CSS3 / Styling, Device Access, Multimedia, Performance &amp; Integration, and Semantics" title="HTML5 Powered with CSS3 / Styling, Device Access, Multimedia, Performance &amp; Integration, and Semantics"></a>
+			</figure>
 		</footer>
 	</body>
 </html>
